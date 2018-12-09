0ba9a68a6120ec5c61ce9ad06678933f615a48ea
Steven Dieu <steven.dieu1@gmail.com>
2018-12-09 14:52:33 +0100

    Fix error engine

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-  res.send('Hello world !');
+router.get('/', function (req, res, err) {
+  res.send('Hello world ! Coucou');
 });
 
 module.exports = router;
