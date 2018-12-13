24f5938d90ef5128671f16c8da28541d2946ae4c
Steven Dieu <steven.dieu1@gmail.com>
2018-12-13 09:31:54 +0100

    Start project with nest

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import {Controller, Get, Req, Res, UseGuards} from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+
+    @Get('google')
+    @UseGuards(AuthGuard('google'))
+    googleLogin() {
+        // initiates the Google OAuth2 login flow
+    }
+
+    @Get('google/callback')
+    @UseGuards(AuthGuard('google'))
+    googleLoginCallback(@Req() req, @Res() res) {
+        // handles the Google OAuth2 callback
+        const jwt: string = req.user.jwt;
+        if (jwt) {
+            res.redirect('http://localhost:4200/login/succes/' + jwt);
+        } else {
+            res.redirect('http://localhost:4200/login/failure');
+        }
+    }
+
+    @Get('protected')
+    @UseGuards(AuthGuard('jwt'))
+    protectedResource(@Req() req) {
+        console.log()
+        return 'JWT is working!';
+    }
+}
